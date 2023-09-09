5d8e74cae07d338f9f7cb3a60d1d4dbac34a98b7
karemcode <karimchu1010@gmail.com>
2023-09-09 13:49:11 +0200

    assets/style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+
+/* Added function to a click menu  */
+
+document.querySelector('#menu').addEventListener('click', () => {
+    document.querySelector('nav ul').classList.toggle('showmenu');
+  });
+  
